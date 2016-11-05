ce43df841950da75889f14c1742fc02a96a24d52
uksspy <uksspylp@gmail.com>
2016-11-05 17:22:15 -0400

    Added MachineLoader class

---
+++ src/main/java/org/devathon/contest2016/storage/MachineLoader.java
@@ -0,0 +1,12 @@
+package org.devathon.contest2016.storage;
+
+import org.devathon.contest2016.DevathonPlugin;
+
+public class MachineLoader {
+
+    private DevathonPlugin plugin;
+
+    public MachineLoader(DevathonPlugin plugin){
+        this.plugin = plugin;
+    }
+}
